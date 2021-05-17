44c54743c6f1d69cb6fd3a48d836e79c5a632486
René Nielsen <renetdev@gmail.com>
2021-05-17 09:30:40 +0200

    added outside tags

--- build/infrastructure/locals.tf
+++ build/infrastructure/locals.tf
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 locals {
+  tags = concat([var.project], data.azurerm_resource_group.main.tags)
 }
